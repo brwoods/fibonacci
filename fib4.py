00caf41eb0ac4437d270e49fe94ef6b396ac5715
Bruce Woods <bruce.woods@verizon.net>
2020-06-20 23:01:58 -0400

    Classic Computer Science Problems in Python; chap 1; fibonacci

---
+++ fib4.py
@@ -0,0 +1,7 @@
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def fib4(n: int) -> int:  # same definition as fib2()    
+    if n < 2:  # base case
+        return n    
+    return fib4(n - 2) + fib4(n - 1)  # recursive case
